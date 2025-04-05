8d94badc1fcc11fdaa34dbc0606a8ffe1f296f04
GoldenSparroww <jankopacka@centrum.cz>
2025-04-05 22:44:52 +0200

    FrameLayout/SideBar atomization (idk if that is good)

--- src/components/FrameLayout.jsx
+++ src/components/FrameLayout.jsx
@@ -1,10 +1,8 @@
 import "../style/FrameLayout.css"
 import BurgerButton from "./BurgerButton.jsx";
 import {useState} from "react";
-import NestedList from "../components/MUI-NestedList.jsx";
-import {ThemeProvider} from '@mui/material/styles';
-import theme from '../global/theme.js';
 import MainSection from "./MainSection.jsx";
+import SideBar from "./Sidebar.jsx";
 
 const FrameLayout = () => {
   const [isSideBarVisible, setIsSideBarVisible] = useState(false);
@@ -16,14 +14,14 @@ const FrameLayout = () => {
         <BurgerButton visible={!isSideBarVisible} onPress={() => setIsSideBarVisible(!isSideBarVisible)}/>
       </div>
 
-      <div id={"side-bar"} className={isSideBarVisible ? "show" : ""}>
-        <BurgerButton visible={isSideBarVisible} onPress={() => setIsSideBarVisible(!isSideBarVisible)}/>
-        <ThemeProvider theme={theme}>
-          <NestedList changeMainSection={setShownMainSection}/>
-        </ThemeProvider>
-        side
-      </div>
+      {/* id={"side-bar"} */}
+      <SideBar
+        setShownMainSection={setShownMainSection}
+        isSideBarVisible={isSideBarVisible}
+        setIsSideBarVisible={setIsSideBarVisible}
+      />
 
+      {/* id={"main-section"} */}
       <MainSection show={shownMainSection}/>
 
 
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,16 @@
-const SideBar = () => {
+import BurgerButton from "./BurgerButton.jsx";
+import {ThemeProvider} from "@mui/material/styles";
+import theme from "../global/theme.js";
+import NestedList from "./MUI-NestedList.jsx";
+
+const SideBar = (props) => {
   return (
-    <div>
-      
+    <div id={"side-bar"} className={props.isSideBarVisible ? "show" : ""}>
+      <BurgerButton visible={props.isSideBarVisible} onPress={() => props.setIsSideBarVisible(!props.isSideBarVisible)}/>
+      <ThemeProvider theme={theme}>
+        <NestedList changeMainSection={props.setShownMainSection}/>
+      </ThemeProvider>
+      side
     </div>
   )
 }
