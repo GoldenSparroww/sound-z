857bea4c0813a66ee29970aecc175d6338d4ad62
GoldenSparroww <jankopacka@centrum.cz>
2025-04-06 18:26:37 +0200

    HomePage some random layout

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,8 +2,11 @@ import "../style/HomePage.css"
 
 const HomePage = () => {
   return (
-    <div>
-
+    <div id={'home-page-container'}>
+      <div id={"tile-recently-played"}>tile-recently-played</div>
+      <div className={"basic-tile"} id={"tile-settings"}>tile-settings</div>
+      <div className={"basic-tile"} id={"tile-about"}>tile-about</div>
+      <div className={"basic-tile"} id={"tile-help"}>tile-help</div>
     </div>
   )
 }
